8dd1db2bd9c5ae870edc265adf94d0f0635bbcbf
itc-s22028 <s22028@std.it-college.ac.jp>
2023-01-20 01:34:35 +0000

    React

--- pages/pop.js
+++ pages/pop.js
@@ -1,19 +1,20 @@
+import Link from 'next/link'
 import styles from 'styles/pop.module.css'
 
 export default function Web () {
   return (
     <ul className={styles.pop_btn}>
       <li>
-        <h2 href="/">Home</h2>
+        <h2><Link href="/">Home</Link></h2>
       </li>
       <li>
-        <h2 href="/">About</h2>
+        <h2><Link href="/">About</Link></h2>
       </li>
       <li>
-        <h2 href="/">Topics</h2>
+        <h2><Link href="/">Topics</Link></h2>
       </li>
       <li>
-        <h2 href="/">Link</h2>
+        <h2><Link href="/">Link</Link></h2>
       </li>
     </ul>
   )
